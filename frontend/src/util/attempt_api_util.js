3ba1a78fc60c14c7021df603dd67ff996b0bcc0c
hiendbui <hiendbui0@gmail.com>
2020-12-30 11:01:54 -0800

    Write requests with axios

---
+++ frontend/src/util/attempt_api_util.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const createAttempt = attempt => {
+    return axios.post('api/attempts', attempt)
+};
+
+export const fetchUserAttempts = userId => {
+    return axios.get(`api/attempts/users/${userId}`)
+};
+
+export const fetchTestAttempts = testId => {
+    return axios.get(`api/attempts/tests/${testId}`)
+};
+
+export const fetchUserTestAttempts = (userId, testId) => {
+    return axios.get(`api/attempts/${userId}/${testId}`)
+};
