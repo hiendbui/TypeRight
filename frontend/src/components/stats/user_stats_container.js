25eac53d3acfbf4d0cb69d01d8913427705dd15e
hiendbui <hiendbui0@gmail.com>
2020-12-31 10:35:08 -0800

    Create user stats container

--- frontend/src/components/stats/user_stats_container.js
+++ frontend/src/components/stats/user_stats_container.js
@@ -1,1 +1,17 @@
 import { connect } from 'react-redux';
+import Stats from './stats';
+import { fetchUserAttempts } from '../../actions/attempt_actions';
+import { fetchTest } from '../../actions/test_actions';
+
+const msp = state => ({
+    currentUser: state.session.user,
+    header: 'Your Overall Stats'
+})
+
+const mdp = dispatch => ({
+    fetchUserAttempts: userId => dispatch(fetchUserAttempts(userId)),
+    fetchTest: id => dispatch(fetchTest(id)),
+})
+
+const UserStatsContainer = connect(msp,mdp)(Stats);
+export default UserStatsContainer;
