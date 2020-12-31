40f279888f528a173ed8b7de226e785a599226b3
hiendbui <hiendbui0@gmail.com>
2020-12-30 16:40:11 -0800

    Finish attempts reducer

---
+++ frontend/src/reducers/attempts_reducer.js
@@ -0,0 +1,19 @@
+import {
+    RECEIVE_ATTEMPTS, 
+    RECEIVE_SINGLE_ATTEMPT
+} from '../actions/attempt_actions';
+
+const initialState = {}
+
+const attemptsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case RECEIVE_ATTEMPTS:
+        return action.attempts;
+    case RECEIVE_SINGLE_ATTEMPT:
+        return {...state, ...action.attempt}
+    default:
+        return state;
+  }
+}
+
+export default attemptsReducer;
--- frontend/src/reducers/entities_reducer.js
+++ frontend/src/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import tests from './tests_reducer';
-import users from './users_reducer'
+import users from './users_reducer';
+import attempts from './attempts_reducer';
 
 const EntitiesReducer = combineReducers({
   users,
-  tests
+  tests,
+  attempts
 });
 
 export default EntitiesReducer;
