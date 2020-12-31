b31b2f15e78fd8fd11942a386ffa08915812b6dc
hiendbui <hiendbui0@gmail.com>
2020-12-31 12:18:56 -0800

    Initialize profile component w/ user stats

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,6 +3,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 import Modal from "./session/modal";
+import Profile from './profile/profile';
 
 import MainPage from './main/main_page';
 
@@ -12,6 +13,7 @@ const App = () => (
     <Modal />
     <div className="app-inner">
       <Switch>
+        <AuthRoute exact path="/profile" component={Profile} />
         <AuthRoute exact path="/" component={MainPage} />
       </Switch>
     </div>
