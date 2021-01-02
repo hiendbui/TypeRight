82b263cc4d2d960515746ffaf9afb4f614b56c83
jbubar <jpbubar@gmail.com>
2021-01-02 13:37:03 -0800

    style main page

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,8 +1,9 @@
-
 import React from 'react';
 import TypeContainer from '../typing/typing_container';
 import LibraryContainer from '../library/library_container';
-import CreateTest from "./create_test"
+import CreateTest from "./create_test";
+import "./main_page.scss";
+
 class MainPage extends React.Component {
 
   render() {
