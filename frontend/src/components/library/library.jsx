e181b5a21f17c5c41dacc6dae8f7be6d5151c0ab
SilentNN <akanam@gmail.com>
2021-01-11 10:43:49 -0800

    user library working

--- frontend/src/components/library/library.jsx
+++ frontend/src/components/library/library.jsx
@@ -3,29 +3,19 @@ import TestItem from './test_item'
 import './library.scss'
 
 export default class Library extends Component {
-    constructor(props){
-        super(props)
-    }
+    
     componentDidMount(){
         this.props.fetchTests(this.props.currentUser);
     }
-    // componentDidUpdate(){
-    //     this.props.fetchLatestTests();
-
-    // }
+    
     render() {
-        if (!this.props.tests || Object.values(this.props.tests).filter(ele => (ele.uploader === this.props.currentUser)).length === 0) return null;
+        if (!this.props.tests) return null;
 
         return (
             <div className="library-container">
                 <h3>{this.props.header}</h3>
                 <div className="page-card library">
-                    {(this.props.tests ? 
-                        this.props.header === "Explore Our Latest Test Submissions" ? 
-                            this.props.tests : 
-                            Object.values(this.props.tests).filter(ele => (ele.uploader === this.props.currentUser)).map(test => test._id) 
-                        : []
-                    ).map( (test, idx) => (
+                    {this.props.tests.map( (test, idx) => (
                         <TestItem key={idx} test={test} editable={this.props.editable}/>
                     ))}
                     <div className="filling-empty-space-childs"></div>
