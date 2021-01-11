768045f5153fd507dab791fe827c8962b650b52d
SilentNN <akanam@gmail.com>
2021-01-11 10:22:13 -0800

    main page updates correctly

--- frontend/src/components/library/library.jsx
+++ frontend/src/components/library/library.jsx
@@ -14,14 +14,20 @@ export default class Library extends Component {
 
     // }
     render() {
-        if (!this.props.tests.latest || Object.values(this.props.tests).filter(ele => (ele.uploader === this.props.currentUser)).length === 0) return null;
+        console.log('tests: ', this.props.tests);
+        if (!this.props.tests || Object.values(this.props.tests).filter(ele => (ele.uploader === this.props.currentUser)).length === 0) return null;
 
         return (
             <div className="library-container">
                 <h3>{this.props.header}</h3>
                 <div className="page-card library">
-                    {(this.props.tests.latest ? this.props.header === "Explore Our Latest Test Submissions" ? this.props.tests.latest : Object.values(this.props.tests).filter(ele => (ele.uploader === this.props.currentUser)).map(test => test._id) : []).map(testId => (
-                        <TestItem key={testId} test={this.props.tests[testId]} editable={this.props.editable}/>
+                    {(this.props.tests ? 
+                        this.props.header === "Explore Our Latest Test Submissions" ? 
+                            this.props.tests : 
+                            Object.values(this.props.tests).filter(ele => (ele.uploader === this.props.currentUser)).map(test => test._id) 
+                        : []
+                    ).map( (test, idx) => (
+                        <TestItem key={idx} test={test} editable={this.props.editable}/>
                     ))}
                     <div className="filling-empty-space-childs"></div>
                     <div className="filling-empty-space-childs"></div>
